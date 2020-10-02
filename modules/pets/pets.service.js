d8ca1bf945c5943a2a4c47ac5dea5ac95c58969b
Hannah <HannahCheyenne@gmail.com>
2020-10-02 11:05:42 -0500

    start server

--- modules/pets/pets.service.js
+++ modules/pets/pets.service.js
@@ -17,9 +17,17 @@ store.dogs.forEach(dog => pets.dogs.enqueue(dog))
 module.exports = {
   get() {
     // Return the pets next in line to be adopted.
+    let cat = pets.cats.showFirst()
+    let dog = pets.dogs.showFirst()
+
+    return {
+      cat,
+      dog
+    }
   },
 
   dequeue(type) {
     // Remove a pet from the queue.
+    return pets[type].dequeue()
   }
 }
